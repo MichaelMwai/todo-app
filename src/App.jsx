7e0476b849fa83e81009ba03ea83c56f4c9c39d8
Michael Mwai <michaeldmwai@gmail.com>
2024-11-14 11:40:58 +0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { Header } from "./components/Header";
+import { Tabs } from "./components/Tabs";
+import { TodoList } from "./components/TodoList";
+import { TodoInput } from "./components/TodoInput";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Tabs />
+      <TodoList />
+      <TodoInput />
+    </>
+  );
+}
+
+export default App;
