b7522a7012d88537b48ac85ebea592d319b969eb
Michael Mwai <michaeldmwai@gmail.com>
2024-11-14 16:55:32 +0300

    Add props.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,16 @@
-export function Header() {
-  return <header></header>;
+export function Header(props) {
+  const { todos } = props;
+  const { userName } = props;
+  const todosLength = todos.length;
+
+  const isTasksPlural = todos.length != 1;
+
+  const taskOrTasks = isTasksPlural ? "tasks" : "task";
+  return (
+    <header>
+      <h1 className="text-gradient">
+        You have {todosLength} open {taskOrTasks}: {userName}
+      </h1>
+    </header>
+  );
 }
