fed1e1b1c53c9d1d46d72fad609e0b1578e9190b
Michael Mwai <michaeldmwai@gmail.com>
2024-11-14 16:56:39 +0300

    Defined tab conditions

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,3 +1,28 @@
-export function Tabs() {
-  return <div></div>;
+export function Tabs(props) {
+  const { todos } = props;
+  const { greating } = props;
+  const tabs = ["All", "Open", "Completed"];
+
+  return (
+    <nav className="tab-container">
+      {tabs.map((tab, tabIndex) => {
+        const numOftasks =
+          tab === "All"
+            ? todos.length
+            : tab === "Open"
+            ? todos.filter((val) => !val.complete).length
+            : todos.filter((val) => val.complete).length;
+
+        return (
+          <button key={tabIndex} className="tab-button">
+            <h4>
+              {tab}
+              <span> {numOftasks}</span>
+            </h4>
+            {greating}
+          </button>
+        );
+      })}
+    </nav>
+  );
 }
