49be15457faf8e3546dd635966a668abc565a432
Michael Mwai <michaeldmwai@gmail.com>
2024-11-14 16:51:22 +0300

    Add todos array for Header.jsx tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import "./App.css";
 import { Header } from "./components/Header";
 import { Tabs } from "./components/Tabs";
 import { TodoList } from "./components/TodoList";
 import { TodoInput } from "./components/TodoInput";
 
 function App() {
+  const todos = [
+    { input: "Hello! Add your first todo!", complete: true },
+    { input: "Get the groceries!", complete: false },
+    { input: "Learn how to web design", complete: false },
+    { input: "Say hi to gran granma", complete: true },
+    { input: "Learn how to web design", complete: false },
+  ];
+
+  const userName = "Michael";
+
   return (
     <>
-      <Header />
-      <Tabs />
-      <TodoList />
+      <Header todos={todos} userName={userName} />
+      <Tabs todos={todos} />
+      <TodoList todos={todos} />
       <TodoInput />
     </>
   );
